2974709cec49e456cdd828aaa5b7c8ba5bb93506
francis-osei <oseifrancis673@gmail.com>
2024-08-30 23:25:37 -0700

    feat: add icons to the button and header

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,10 @@
 export default function Button({ text }: { text: string }) {
   return (
-    <button className="font-gilroy font-semibold text-[1.8rem] py-[.7rem] px-[1.6rem] rounded-[1rem] leading-[4.2rem] text-white bg-black">
+    <button className="flex items-center gap-[.4rem] font-gilroy font-semibold text-[1.8rem] py-[.7rem] px-[1.6rem] rounded-[1rem] leading-[4.2rem] text-white bg-black">
       {text}
+      <svg className="flex items-center w-[1.7rem] h-[1.5rem]">
+        <use xlinkHref={`/icons/svg/sprite.svg#icon-arrow-right`}></use>
+      </svg>
     </button>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,12 @@ export default function Header() {
     <div className="flex justify-center items-center mt-[3.9rem] mb-[3.3rem] mx-auto w-full">
       <div className="flex gap-[34.2rem]">
         <Logo />
-        <Nav />
+        <div className="flex gap-[4rem]">
+          <Nav />
+          <svg className="flex items-center w-[2.4rem] h-[2.4rem]">
+            <use xlinkHref={`/icons/svg/sprite.svg#icon-search`}></use>
+          </svg>
+        </div>
       </div>
     </div>
   );
