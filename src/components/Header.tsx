0e38166bbe951a0358e7aed29e0a58e91e0a7965
francis-osei <oseifrancis673@gmail.com>
2024-09-01 07:54:07 -0700

    fix: fixes #6

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className=" flex justify-center items-center mt-[3.9rem] mb-[3.3rem] mx-auto w-full">
       <div className="max-[1216px]:gap-[10rem] flex gap-[34.2rem]">
         <Logo />
-        <div className="flex gap-[4rem] hide">
+        <div className="flex gap-[4rem] max-[972px]:hidden">
           <Nav />
           <svg className="flex items-center w-[2.4rem] h-[2.4rem]">
             <use xlinkHref={`/icons/svg/sprite.svg#icon-search`}></use>
